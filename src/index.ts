30d23d2cc3ab0e03312a3d587c12caa14bf11082
Jujhar Singh <jujhar@jujhar.com>
2024-07-03 14:23:42 +0100

    adds github pages build

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import './index.css';
 
 document.querySelector('#root')!.innerHTML = `
+<div class="header">
+  <h1>Rsbuild</hjson>
+  <p>Rsbuild is a simple build tool for web development.</p>
 <div class="content">
   <h1>Vanilla Rsbuild</h1>
   <p>Start building amazing things with Rsbuild.</p>
