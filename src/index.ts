1df9be60a79055819c33e6c2b3990e86df3b96db
Jujhar Singh <jujhar@jujhar.com>
2024-07-03 14:26:01 +0100

    adds github pages build

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import './index.css';
 
+document.title="Gurdwara Room display panel"
+
 document.querySelector('#root')!.innerHTML = `
-<div class="header">
-  <h1>Rsbuild</hjson>
-  <p>Rsbuild is a simple build tool for web development.</p>
 <div class="content">
   <h1>Vanilla Rsbuild</h1>
   <p>Start building amazing things with Rsbuild.</p>
