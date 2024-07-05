adb5dcfed9e4b1c3cc34f044eb448dcc7852ac03
Jujhar Singh <jujhar@jujhar.com>
2024-07-05 23:26:14 +0100

    adds in animation for event type

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,16 @@ import "./index.css";
 import { display } from "./display";
 import { eventType } from "./eventType";
 
+// Get screen and source from GET params in the URL - set defaults if not passed in
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
+const source = urlParams.get("source")
+  ? urlParams.get("source")
+  : "2PACX-1vTyKxo6YZo0vBJ1Z0ZXItkC1IEs4I8B8Xc178KcSW461qwxfnEyzqcXuU_cY1iVc3ShNPW3oQYPFFrz";
+const screen = urlParams.get("screen") ? urlParams.get("screen") : "1";
 
 let content;
 try {
-  const source = urlParams.get("source");
-  const screen = urlParams.get("screen");
-
   if (source !== null && screen !== null) {
     content = await display.getContent(source, parseInt(screen));
   } else {
@@ -49,8 +51,8 @@ imageElement.style.backgroundImage = `url(${image})`;
 contentDiv!.innerHTML = `
   <p id='date'>${content?.date}</p>
   ${imageElement.outerHTML}
-  <p id='type'>${content?.type}</p>
-  <h1 id='title'>${content?.title}</h1>
+  <div id="typing-effect-container"><p id='eventType'>${content?.eventType}</p></div>
+  <h1 id='title' class='typing-effect'>${content?.title}</h1>
   <h2 id='subtitle'>${content?.subtitle}</h2>
   <p id='misc'>${content.misc}</p>
   <p id='time'>${content?.time}</p>
