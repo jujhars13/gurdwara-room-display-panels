78ee67e159c845fe77d47f998baaada8cd82f28e
Jujhar Singh <jujhar@jujhar.com>
2024-07-05 13:20:00 +0100

    forgot to add in eventtype.ts

---
+++ src/eventType.ts
@@ -0,0 +1,39 @@
+const eventType = {
+  default: {
+    img: "https://i1.sndcdn.com/artworks-000094076123-dtg1vr-t500x500.jpg"
+  },
+  "Akhand Paath": {
+    img: "https://i1.sndcdn.com/artworks-000094076123-dtg1vr-t500x500.jpg"
+  },
+  "Sukhmani Sahib": {
+    img: "https://i1.sndcdn.com/artworks-000094076123-dtg1vr-t500x500.jpg"
+  },
+  Jaap: { img: "https://kaurlife.org/wp-content/uploads/2014/12/n.jpg" },
+  "Keertan Darbaar": {
+    img: "https://kaurlife.org/wp-content/uploads/2014/12/n.jpg"
+  },
+  "Gurbani Class": {
+    img: "https://kaurlife.org/wp-content/uploads/2014/12/n.jpg"
+  },
+  "Keertan Class": {
+    img: "https://kaurlife.org/wp-content/uploads/2014/12/n.jpg"
+  },
+  Workshop: { img: "https://kaurlife.org/wp-content/uploads/2014/12/n.jpg" },
+  Party: { img: "https://kaurlife.org/wp-content/uploads/2014/12/n.jpg" },
+  "Birthday Party": {
+    img: "https://kaurlife.org/wp-content/uploads/2014/12/n.jpg"
+  },
+  "Anand Karaj": {
+    img: "https://kaurlife.org/wp-content/uploads/2014/12/n.jpg"
+  },
+  "Punjabi class": {
+    img: "https://kaurlife.org/wp-content/uploads/2014/12/n.jpg"
+  },
+  Yoga: { img: "https://kaurlife.org/wp-content/uploads/2014/12/n.jpg" },
+  Funeral: { img: "https://kaurlife.org/wp-content/uploads/2014/12/n.jpg" },
+  Paath: {
+    img: "https://i1.sndcdn.com/artworks-000094076123-dtg1vr-t500x500.jpg"
+  }
+};
+
+export { eventType };
