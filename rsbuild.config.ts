6e1c8cf239a63d59c26ca912e0bb37d0c6092971
Jujhar Singh <jujhar@jujhar.com>
2024-10-13 09:35:12 +0100

    parking

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from "@rsbuild/core";
 
 export default {
+  server: {
+    port: 3001
+  },
   html: {
-    title: "Gurdwara Display Panels"
+    title: "Gurdwara Display Panels",
+    template: "./src/index.html"
+
   },
   output: {
     copy: [
-      // `./src/assets/image.png` -> `./dist/image.png`
+      { from: "./src/favicon.ico", to: "" },
+      { from: "./src/css", to: "static/css" },
       { from: "./src/images", to: "static/images" }
     ]
   }
